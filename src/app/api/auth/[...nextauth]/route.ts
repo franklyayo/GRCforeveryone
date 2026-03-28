c7c89b6e8247f4894ab468dda71fcf5a2ff04322
franklyayo <149245900+franklyayo@users.noreply.github.com>
2026-03-28 19:52:34 +0100

    Add dynamic export to NextAuth route
    Added dynamic export to force dynamic rendering.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,7 @@
 // src/app/api/auth/[...nextauth]/route.ts
+
+export const dynamic = "force-dynamic"; // <-- Add this line
+import clientPromise from "@/lib/mongoClient";
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
